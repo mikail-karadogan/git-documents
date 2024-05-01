358d5012f0ccd6e7f604353ee30678f99bdc94d4
mikail-karadogan-ntt <mikail.karadogan@bs.nttdata.com>
2024-05-01 11:08:53 +0300

    trial 1

--- index.ts
+++ index.ts
@@ -2,4 +2,18 @@ function trial() {
     return 1;
 }
 
+function trial2() {
+    return 2;
+}
+function trial3() {
+    return 3;
+}
+
+function trial4() {
+    return 4;
+}
+
+function trial5() {
+    return 5;
+}
 console.log(trial());
